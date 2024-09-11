c4195c6773a5e286ae06f00747ff79699ffbf3c0
Jo-Zh <joanna.zhong@uq.edu.au>
2024-09-11 16:03:59 +1000

    prevent button to trigger save

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { Item } from "./type";
 // generate inital position and dimension for question options
 export const getInitialVal = (num: number) => {
   return Array.from({ length: num }, (_, index) => {
-    return { id: index, x: index * 5, y: index * 30, width: 100, height: 100 };
+    return { id: index, x: index * 30, y: index * 30, width: 100, height: 30 };
   });
 };
 
