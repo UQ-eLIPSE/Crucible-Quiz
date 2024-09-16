92af90d2dd0281c9aa3baf026ac2eb43271f3e89
Joanna Zhong <joanna.zhong@uq.edu.au>
2024-09-16 11:40:08 +1000

    update

--- tests/Util.test.ts
+++ tests/Util.test.ts
@@ -1,16 +1,15 @@
 import { describe, it, expect } from "vitest";
 import { base64ToBlob, base64ToFile } from "../src/utils";
 
-  it("should convert base64 string to a Blob with the correct MIME type", () => {
-    const base64String =
-      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
-    const mimeType = "image/png";
-    const blob = base64ToBlob(base64String, mimeType);
+it("should convert base64 string to a Blob with the correct MIME type", () => {
+  const base64String =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
+  const mimeType = "image/png";
+  const blob = base64ToBlob(base64String, mimeType);
 
-    expect(blob).toBeInstanceOf(Blob);
-    expect(blob.type).toBe(mimeType);
-    expect(blob.size).toBeGreaterThan(0);
-  });
+  expect(blob).toBeInstanceOf(Blob);
+  expect(blob.type).toBe(mimeType);
+  expect(blob.size).toBeGreaterThan(0);
 });
 
 describe("base64ToFile", () => {
